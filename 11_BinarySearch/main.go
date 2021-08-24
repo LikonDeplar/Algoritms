64d8c99c551abc7a62a7b3e3d23673820724dfb5
ilya.tarasov <likondeplar@icloud.com>
2021-08-24 20:51:36 +0300

    fix condition in BinarySearch

--- 11_BinarySearch/main.go
+++ 11_BinarySearch/main.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{-100, -50, -40, 10, 20, 30, 40, 50, 60, 70, 80, 90 ,100}
-	fmt.Println(BinarySearch(-110, &a))
+	a := []int{-100, -50, -40, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
+	fmt.Println(BinarySearch(-1990, &a))
 }
 
 func BinarySearch(num int, arr *[]int) int {
 	low := 0
-	end := len(*arr)-1
-	for {
+	end := len(*arr) - 1
+	for low <= end {
 		mid := (low + end) / 2
 		if (*arr)[mid] == num {
 			return mid
@@ -19,9 +19,6 @@ func BinarySearch(num int, arr *[]int) int {
 		} else if (*arr)[mid] < num {
 			low = mid + 1
 		}
-
-		if low > end {
-			return -1
-		}
 	}
+	return -1
 }
